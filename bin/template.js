776d7b6b926aba259e9ce4dd84c10a7e4fdcc75c
gants <656828947@qq.com>
2020-07-10 11:38:08 +0800

    Update template.js

--- bin/template.js
+++ bin/template.js
@@ -3,7 +3,6 @@ const getAttrs = (style) => {
     'xmlns': 'http://www.w3.org/2000/svg',
     ':width': 'size',
     ':height': 'size',
-    'viewBox': '0 0 24 24',
     'aria-hidden': 'true',
     'v-on': '$listeners'
   }
